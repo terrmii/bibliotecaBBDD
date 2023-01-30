b6bb0713c71363a009fdf6649b111d85327c140f
plaiaundi <plaiaundi@HZ309807.H012108.NET>
2023-01-30 14:15:44 +0100

    esqueleto hecho faltan metodos

--- src/main/GestorSocios.java
+++ src/main/GestorSocios.java
@@ -2,8 +2,25 @@ package main;
 
 import java.util.Scanner;
 
+import clases.Menu;
+
 public class GestorSocios {
 	public static void run(Scanner scan) {
-		
+		int opcion;
+		do {
+			Menu.mostrarMenuSocios();
+			opcion = Integer.parseInt(scan.nextLine());
+			
+			
+			switch (opcion) {
+			case Menu.VER_SOCIOS: 
+				System.out.println("Socios: ");
+				
+			
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + opcion);
+			}
+		} while (opcion != 0);
+
 	}
 }
