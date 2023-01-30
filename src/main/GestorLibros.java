f897c34d28a2760da5ceb7f83df9d378ddb50c43
plaiaundi <plaiaundi@HZ309807.H012108.NET>
2023-01-30 14:11:37 +0100

    llamada a insertar libro y eliminar de menu

--- src/main/GestorLibros.java
+++ src/main/GestorLibros.java
@@ -1,5 +1,27 @@
 package main;
 
+import java.util.Scanner;
+
+import clases.Menu;
+
 public class GestorLibros {
+	public static void run(Scanner scan) {
+		int opcion;
+		do {
+			Menu.mostrarMenuLibros();
+			opcion = Integer.parseInt(scan.nextLine());
+			
+			switch (opcion) {
+			case Menu.INSERTAR_LIBRO: 
+				System.out.println("ins socio");
+			
+			case Menu.ELIMINAR_LIBRO: 
+				System.out.println("ins socio");
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + opcion);
+			}
+			
+		} while (opcion != 0);
+	}
 
 }
