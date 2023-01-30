2e70abd10bf80a85a22087d2f66227b6757aa4a3
plaiaundi <plaiaundi@HZ309807.H012108.NET>
2023-01-30 13:17:02 +0100

    insertar y eliminar hecho

--- src/main/GestorBBDD.java
+++ src/main/GestorBBDD.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import clases.Libro;
 
 /**
@@ -13,12 +16,21 @@ public class GestorBBDD {
 	Conector conector = new Conector();
 	
 
-	public void insertarLibro(Libro libro) {
-		
+	public void insertarLibro(Libro libro) throws ClassNotFoundException, SQLException {
+		Conector con = new Conector();
+		con.conectar();
+		PreparedStatement insertar = con.getCon().prepareStatement("INSERT INTO libros (titulo, autor, num_pag) VALUES (?, ?, ?) ");
+		insertar.setString(1,  libro.getTitulo());
+		insertar.setString(2, libro.getAutor());
+		insertar.setInt(3, libro.getNumPag());
+		insertar.execute();
 	}
 	
-	public void eliminarLibro(int id) {
-		
+	public void eliminarLibro(int id) throws ClassNotFoundException, SQLException {
+		Conector con = new Conector();
+		con.conectar();
+		PreparedStatement eliminar = con.getCon().prepareStatement("DELETE FROM libros WHERE `libros`.`id` = ?");
+		eliminar.setInt(1, id);
 	}
 	
 	public Libro getLibro(int id) {
